da780042c56f2247fd050ef7d9d383524614265b
ArthurAleqsan <aleqsanyanarthur@gmail.com>
2019-07-03 16:17:08 +0400

    remove favicon link and fix path to loader

--- src/index.jsx
+++ src/index.jsx
@@ -4,7 +4,7 @@ import { Provider } from 'react-redux';
 import { Router } from 'react-router-dom';
 import store, { history } from './store/configureReduxStore';
 import Routes from './containers/router';
-import Loader from './components/componentsLib/Loader';
+import Loader from './components/componentsLib/simpleUIComponents/Loader';
 import './../assets/styles/index.scss';
 import './i18n';
 render((
